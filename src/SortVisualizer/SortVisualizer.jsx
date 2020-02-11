f33bf1bc94f9e7c51ff830faec7f22bc0af918c4
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2020-02-11 22:25:20 +0000

    Change class component to functional component

--- src/SortVisualizer/SortVisualizer.jsx
+++ src/SortVisualizer/SortVisualizer.jsx
@@ -1,105 +1,94 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import "./SortVisualizer.css";
 
 const ARR_LEN = 100;
 const MIN_NUM = 5;
 const MAX_NUM = 80;
 
-export default class SortVisualizer extends Component {
-  constructor(props) {
-    super(props);
+export default function SortVisualizer(props) {
+  const [arr, updateArr] = useState([]);
 
-    this.state = { arr: [] };
-    this.initialiseArray = this.initialiseArray.bind(this);
-    this.mergeSort = this.mergeSort.bind(this);
-    this.insertionSort = this.insertionSort.bind(this);
-  }
+  useEffect(initialiseArray, []);
 
-  componentDidMount() {
-    this.initialiseArray();
-  }
-
-  initialiseArray() {
+  function initialiseArray() {
     const arr = [];
     for (let i = 0; i < ARR_LEN; i++) {
       arr.push(randIntRange(MIN_NUM, MAX_NUM));
     }
-    this.setState({
-      arr: arr
-    });
+    updateArr(arr);
   }
 
-  mergeSort() {
-    const copy = this.state.arr.slice();
+  function mergeSort() {
+    const copy = arr.slice();
     const len = copy.length;
     const aux = Array(len);
-    this.mergeSortHelper(copy, aux, 0, len - 1);
-    this.setState({
-      arr: copy
-    });
+    const animations = [];
+    mergeSortHelper(copy, aux, 0, len - 1, animations);
+    updateArr(copy);
+    return animations;
   }
 
-  mergeSortHelper(arr, aux, left, right) {
+  function mergeSortHelper(arr, aux, left, right, animations) {
     if (right <= left) return;
     const mid = left + Math.floor((right - left) / 2);
-    this.mergeSortHelper(arr, aux, left, mid);
-    this.mergeSortHelper(arr, aux, mid + 1, right);
-    this.merge(arr, aux, left, mid, right);
+    mergeSortHelper(arr, aux, left, mid, animations);
+    mergeSortHelper(arr, aux, mid + 1, right, animations);
+    merge(arr, aux, left, mid, right, animations);
   }
 
-  merge(arr, aux, left, mid, right) {
+  function merge(arr, aux, left, mid, right, animations) {
     for (let i = left; i <= right; i++) aux[i] = arr[i];
     let i = left;
     let j = mid + 1;
     for (let k = left; k <= right; k++) {
+      animations.push([i, j]);
       if (i > mid) arr[k] = aux[j++];
       else if (j > right) arr[k] = aux[i++];
       else if (aux[j] < aux[i]) arr[k] = aux[j++];
       else arr[k] = aux[i++];
+      animations.push(k);
     }
   }
 
-  insertionSort() {
-    const copy = this.state.arr.slice();
+  function insertionSort() {
+    const copy = arr.slice();
     for (let i = 1; i < copy.length; i++) {
       for (let j = i - 1; j >= 0; j--) {
         if (copy[j + 1] < copy[j]) swap(copy, j, j + 1);
         else break;
       }
     }
-    this.setState({
-      arr: copy
-    });
+    updateArr(copy);
   }
 
-  render() {
-    const { arr } = this.state;
-
-    return (
-      <>
-        <div className="array-container">
-          {arr.map((barHeight, index) => (
-            <div
-              className="array-bar"
-              style={{ height: `${barHeight}vmin` }}
-              key={index}
-            ></div>
-          ))}
-        </div>
-        <footer>
-          <button className="app-button" onClick={this.initialiseArray}>
-            Create new array
-          </button>
-          <button className="app-button" onClick={this.mergeSort}>
-            Merge sort
-          </button>
-          <button className="app-button" onClick={this.insertionSort}>
-            Insertion sort
-          </button>
-        </footer>
-      </>
-    );
-  }
+  return (
+    <>
+      <div className="array-container">
+        {arr.map((barHeight, index) => (
+          <div
+            className="array-bar"
+            style={{ height: `${barHeight}vmin` }}
+            key={index}
+          ></div>
+        ))}
+      </div>
+      <footer>
+        <button className="app-button" onClick={initialiseArray}>
+          Create new array
+        </button>
+        <button className="app-button" id="merge-sort" onClick={mergeSort}>
+          Merge sort
+        </button>
+        <button
+          className="app-button"
+          id="insertion-sort"
+          onClick={insertionSort}
+        >
+          Insertion sort
+        </button>
+      </footer>
+    </>
+  );
 }
 
 const randIntRange = (start, end) =>
