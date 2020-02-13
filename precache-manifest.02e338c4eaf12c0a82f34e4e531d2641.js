cca5573cd5561e4225d9eed6f46ae0b7b3a3203b
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2020-02-13 03:19:28 +0000

    Updates

---
+++ precache-manifest.02e338c4eaf12c0a82f34e4e531d2641.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7907390bcb5d99d44a7975024d03ffba",
+    "url": "/react-sort-visualizer/index.html"
+  },
+  {
+    "revision": "c3a57ffa00485d7e127f",
+    "url": "/react-sort-visualizer/static/css/main.89eabed0.chunk.css"
+  },
+  {
+    "revision": "e2b21c43b07612d5f614",
+    "url": "/react-sort-visualizer/static/js/2.61aff7ff.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-sort-visualizer/static/js/2.61aff7ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3a57ffa00485d7e127f",
+    "url": "/react-sort-visualizer/static/js/main.b5495767.chunk.js"
+  },
+  {
+    "revision": "b1f21fa4ec76fcd3c17c",
+    "url": "/react-sort-visualizer/static/js/runtime-main.2dd63f75.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-sort-visualizer/precache-manifest.02e338c4eaf12c0a82f34e4e531d2641.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-sort-visualizer/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
